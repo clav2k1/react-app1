dae3fc30c0ef8ef43334d4133d446f7cbe07f48d
clav2k1 <clav2k1@hotmail.com>
2020-10-02 22:23:39 -0300

    cambio de estado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function HellWorld(props) {
-  console.log(props);
+//// componente como funcion
+// function HellWorld(props) {
+//   console.log(props);
   
-  return (
-  <div id="hello">
-    <h3>{props.head}</h3>
-    Hello {props.txt}
-  </div>
-  );
+//   return (
+//   <div id="hello">
+//     <h3>{props.head}</h3>
+//     Hello {props.txt}
+//   </div>
+//   );
+// }
+
+// componente como clase
+class HellWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      
+      return (
+        <div id="hello">
+          <h3>{this.props.head} con Clase</h3>
+          Hello {this.props.txt}
+          <button onClick={ this.toggleShow }>Cambia Estado</button>
+        </div>
+        );
+
+    } else {
+      return <h1>No hay Informacion <button onClick={ this.toggleShow }>Cambia Estado</button></h1>
+    }
+  }
 }
+
 
 // llamado del componente como funcion =>
 // const App = () => <div>Este es mi componente: <HellWorld/> con Flecha</div>
@@ -31,7 +60,7 @@ function App() {
       Este es mi componente: 
       <HellWorld txt="clav2k1" head="Component 1"/> 
       <HellWorld txt="Clau" head="Component 2"/> 
-      <HellWorld txt="Javi"/> con Funcion
+      <HellWorld txt="Javi"/> 
     </div>
   );
 }

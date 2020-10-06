5abbf15ddf79954aad8a0f766cb7fc7044d5e7ac
clav2k1 <clav2k1@hotmail.com>
2020-10-06 19:24:22 -0300

    estilos con archivo css, directo y con condicional

--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import './task.css';
 
 
 class Task extends Component {
+    styleTaskCompleted() {
+        return {
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray' : 'red',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+    
     render() {
-        // return <div>
+        // return <div  className="red">
         //     {this.props.task.title} - 
         //     {this.props.task.description} - 
         //     {this.props.task.done} - 
@@ -13,14 +22,24 @@ class Task extends Component {
         // </div>
 
         const {task} = this.props;
+        
+        const styleBtnDelete = {
+            fontSize: '18px',
+            color: '#fff',
+            background: 'red',
+            border: 'none',
+            padding: '10px 15px',
+            borderRadius: '50%',
+            cursor: 'pointer'
+        };
 
-        return <div>
+        return <div style={this.styleTaskCompleted()}>
             {task.title} - 
             {task.description} - 
             {task.done} - 
             {task.id}
             <input type="checkbox" />
-            <button>x</button>
+            <button style={styleBtnDelete}>x</button>
         </div>
     }
 }
